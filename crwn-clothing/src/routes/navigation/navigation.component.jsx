510263184ff91352ad1349f581c95c2cf92f49f0
Nicu Staris <5starn95@solent.ac.uk>
2023-05-03 22:15:21 +0100

    added addToCart feature

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -13,7 +13,7 @@ import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  const { isCartOpen } = useContext(CartContext);
+  let { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
